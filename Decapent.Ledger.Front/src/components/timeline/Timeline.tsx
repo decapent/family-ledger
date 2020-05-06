ca9d58f0e6aea335fef84e364a12298766d4ca57
Patrick Lavallee <patrick.lavallee@nexusinno.com>
2020-05-06 16:01:30 -0400

    dummy data

--- Decapent.Ledger.Front/src/components/timeline/Timeline.tsx
+++ Decapent.Ledger.Front/src/components/timeline/Timeline.tsx
@@ -18,9 +18,9 @@ export default class Timeline extends React.Component<ITimelineProps, ITimelineS
 
         let event: LedgerEvent = {
             Author: "Autheur",
-            City: "Ville",
+            City: "Saint-Paul de l'ile au noix",
             Date: "1850/04/12",
-            Description: "Description",
+            Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus at viverra purus, sed vulputate lorem. Aliquam est nunc, iaculis non leo vel, tristique vestibulum libero. Quisque et tempor ex. Sed vestibulum lorem ac lacus rhoncus, at iaculis lorem consectetur. Sed condimentum porta arcu pharetra auctor. Sed at volutpat dui, quis hendrerit nisi. Maecenas scelerisque suscipit lacus, quis molestie nibh aliquet vitae. Vivamus nisi turpis, euismod at magna ut, ultricies ultrices quam. Etiam eu hendrerit arcu, ut laoreet massa. In erat nunc, dapibus non velit vitae, maximus ullamcorper quam. Cras turpis dolor, venenatis interdum velit id, congue porta massa. Cras blandit dui eu magna tempus porta. Aenean vitae mi consectetur, ultricies leo at, auctor nulla. Vivamus eu magna a metus pulvinar tincidunt.",
             Id: "MonId",
             LedgerImage: "Une image",
             LedgerPage: 42,
