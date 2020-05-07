b1ab0c76da4ab179c55ca5a8da22e3f1dabf4fdc
decapent <patrick.lavallee@nexusinno.com>
2020-05-07 12:00:33 -0400

    removed unused import statement

--- Decapent.Ledger.Front/src/components/app.tsx
+++ Decapent.Ledger.Front/src/components/app.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { VerticalTimeline, VerticalTimelineElement  } from 'react-vertical-timeline-component';
 import Timeline from "./timeline/Timeline";
 
 // Styles
